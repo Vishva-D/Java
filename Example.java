cea9caf7905f3f9b9d89f39329fe9c782bf28a66
Vishva-D <58882748+Vishva-D@users.noreply.github.com>
2021-04-09 08:56:28 +0530

    Created sample program

---
+++ Example.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        int a1 = 10;
+         int a2 = 26;
+         int a3 = 16;
+          int a4 = 18;
+          int result1 = a1-a2/a3;
+          int result2 = (a1-a2)/a3;
+          System.out.println(result1);
+          System.out.println(result2);
+    }
+}
